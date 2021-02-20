6ef617b443956c0b06801045138d6a947d9ef49d
jhlee <leejh8372@gmail.com>
2021-02-20 23:41:59 +0900

    change image url

--- src/bg.js
+++ src/bg.js
@@ -2,9 +2,18 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 6;
 
+const IMG_SRCS = [
+  "https://i.ibb.co/M280nPC/1.jpg",
+  "https://i.ibb.co/b6WLCb0/2.jpg",
+  "https://i.ibb.co/7vyJXWT/3.jpg",
+  "https://i.ibb.co/xjbLkFY/4.jpg",
+  "https://i.ibb.co/gjvCJDd/5.jpg",
+  "https://i.ibb.co/kHTYBtC/6.jpg",
+]
+
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `images/${imgNumber + 1}.jpg`;
+  image.src = IMG_SRCS[imgNumber];
   image.classList.add("bgImage");
   body.prepend(image);
 }
